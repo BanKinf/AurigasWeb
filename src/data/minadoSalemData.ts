46816ba1a2e253de0fd22ff44223f1888b3ef045
_ignacio.dev_ <protec83007@gmail.com>
2025-03-01 06:58:37 -0300

    new section

---
+++ src/data/minadoSalemData.ts
@@ -0,0 +1,11 @@
+export const MinadoSalemData = [
+    [1, '04:49:59', '100.444', '100.444', 'https://www.twitch.tv/videos/2379242490?t=01h06m18s'],
+    [2, '04:31:50', '123.583', '224.040', 'https://www.twitch.tv/videos/2383707630?t=00h40m04s'],
+    [3, '05:03:16', '132.982', '357.562', 'https://www.twitch.tv/videos/2385477728?t=00h08m40s'],
+    [4, '03:41:31', '87.811', '445.373', 'https://www.twitch.tv/videos/2386353960?t=00h09m04s'],
+    [5, '05:02:52', '101.600', '547.103', 'https://www.twitch.tv/videos/2389984933?t=00h08m25s'],
+    [6, '05:25:02', '130.006', '677.109', 'https://www.twitch.tv/videos/2390820015?t=00h50m01s'],
+    [7, '05:00:16', '140.286', '817.528', 'https://www.twitch.tv/videos/2391658644?t=00h35m44s'],
+    [8, '04:57:28', '34.117', '851.833', 'https://www.twitch.tv/videos/2392500046?t=00h07m02s'],
+    [9, '05:15:52', '147.757', '1.000.133', 'https://www.twitch.tv/videos/2393380564?t=00h30m13s']
+];
