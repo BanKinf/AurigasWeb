95286a5772dd6c73ae2b50eeace6f67faf93cec0
_ignacio.dev_ <protec83007@gmail.com>
2025-03-04 03:56:29 -0300

    fix url

--- src/data/minadoSalemData.ts
+++ src/data/minadoSalemData.ts
@@ -8,5 +8,5 @@ export const MinadoSalemData = [
     [7, '04:21:25', '140.286', '817.528', 'https://www.twitch.tv/videos/2391658644?t=00h35m44s'],
     [8, '02:37:47', '34.117', '851.833', 'https://www.twitch.tv/videos/2392500046?t=00h07m02s'],
     [9, '04:40:15', '147.757', '1.000.133', 'https://www.twitch.tv/videos/2393380564?t=00h30m13s'],
-    [10, '04:01:16', '150.436', '1.150.576', 'https://www.twitch.tv/videos/2393380564?t=00h30m13s'],
+    [10, '04:01:16', '150.436', '1.150.576', 'https://www.twitch.tv/videos/2396098946?t=00h30m11s'],
 ];
