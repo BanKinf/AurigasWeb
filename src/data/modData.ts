ebf3e926d51dc3e3f97fb87be5988eaa3273df83
_ignacio.dev_ <protec83007@gmail.com>
2025-03-07 08:54:21 -0300

    upgrade ui/ux and agregate new mod

--- src/data/modData.ts
+++ src/data/modData.ts
@@ -1,75 +1,81 @@
 export interface ModData {
-    name: string;
-    link: string;
-    author: string;
-    image: string;
+  name: string;
+  link: string;
+  author: string;
+  image: string;
 }
   
 export const mods: ModData[] = [
-    {
-      name: "ReplayMod",
-      link: "https://modrinth.com/mod/replaymod",
-      author:"Johni0702",
-      image: "/images/mods/replaymod.webp",
-    },
-    {
-      name: "CarpetMod",
-      link: "https://modrinth.com/mod/carpet",
-      author:"Gnembon",
-      image: "/images/mods/carpetmod.webp",
-    },
-    {
-      name: "FreeCam",
-      link: "https://modrinth.com/mod/freecam",
-      author:"Hashalite",
-      image: "/images/mods/freecam.webp",
-    },
-    {
-      name: "Minihud",
-      link: "/images/mods/replaymod.webp",
-      author:"Masa",
-      image: "/images/mods/minihud.webp",
-    },
-    {
-      name: "Lithium",
-      link: "https://modrinth.com/mod/lithium",
-      author:"Jellysquid3",
-      image: "/images/mods/lithium.webp",
-    },
-    {
-      name: "Starlight",
-      link: "https://modrinth.com/mod/starlight",
-      author:"Spottedleaf",
-      image: "/images/mods/starlight.webp",
-    },
-    {
-      name: "Sodium",
-      link: "https://modrinth.com/mod/sodium",
-      author:"Jellysquid3",
-      image: "/images/mods/sodium.webp",
-    },
-    {
-      name: "Entity Culling",
-      link: "https://modrinth.com/mod/entityculling",
-      author:"Tr7zw",
-      image: "/images/mods/entity-culling.webp",
-    },
-    {
-      name: "Entity Render Disabler",
-      link: "https://modrinth.com/mod/entity-render-disabler",
-      author:"Ponchisao",
-      image: "/images/mods/entity-render-disabler.webp",
-    },
-    {
-      name: "Nvidium",
-      link: "https://modrinth.com/mod/nvidium",
-      author:"Cortex",
-      image: "/images/mods/nvidium.webp",
-    },
-    {
-      name: "WeatherHud",
-      link: "https://github.com/RLLD576/WeatherHud/releases/tag/V1.0.3",
-      author:"RLLD576",
-      image: "/images/mods/weather-hud.webp",
-    },
+  {
+    name: "ReplayMod",
+    link: "https://modrinth.com/mod/replaymod",
+    author:"Johni0702",
+    image: "/images/mods/replaymod.webp",
+  },
+  {
+    name: "CarpetMod",
+    link: "https://modrinth.com/mod/carpet",
+    author:"Gnembon",
+    image: "/images/mods/carpetmod.webp",
+  },
+  {
+    name: "FreeCam",
+    link: "https://modrinth.com/mod/freecam",
+    author:"Hashalite",
+    image: "/images/mods/freecam.webp",
+  },
+  {
+    name: "Minihud",
+    link: "/images/mods/replaymod.webp",
+    author:"Masa",
+    image: "/images/mods/minihud.webp",
+  },
+  {
+    name: "Lithium",
+    link: "https://modrinth.com/mod/lithium",
+    author:"Jellysquid3",
+    image: "/images/mods/lithium.webp",
+  },
+  {
+    name: "Starlight",
+    link: "https://modrinth.com/mod/starlight",
+    author:"Spottedleaf",
+    image: "/images/mods/starlight.webp",
+  },
+  {
+    name: "Sodium",
+    link: "https://modrinth.com/mod/sodium",
+    author:"Jellysquid3",
+    image: "/images/mods/sodium.webp",
+  },
+  {
+    name: "Sodium Extra",
+    link: "https://modrinth.com/mod/sodium-extra",
+    author:"FlashyReese",
+    image: "/images/mods/sodium-extra.webp",
+  },
+  {
+    name: "Nvidium",
+    link: "https://modrinth.com/mod/nvidium",
+    author:"Cortex",
+    image: "/images/mods/nvidium.webp",
+  },
+  {
+    name: "Entity Culling",
+    link: "https://modrinth.com/mod/entityculling",
+    author:"Tr7zw",
+    image: "/images/mods/entity-culling.webp",
+  },
+  {
+    name: "Entity Render Disabler",
+    link: "https://modrinth.com/mod/entity-render-disabler",
+    author:"Ponchisao",
+    image: "/images/mods/entity-render-disabler.webp",
+  },
+  {
+    name: "WeatherHud",
+    link: "https://github.com/RLLD576/WeatherHud/releases/tag/V1.0.3",
+    author:"RLLD576",
+    image: "/images/mods/weather-hud.webp",
+  },
 ];  
