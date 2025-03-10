c0e9b8f01ccb8b02c1c4dc18a0e58a2947d14d50
_ignacio.dev_ <protec83007@gmail.com>
2025-03-10 12:51:16 -0300

    add href to bottom

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -3,24 +3,28 @@ export const news = [
     title: "PROYECTO SALEM",
     description: "Aurigas a dado comienzo a un proyecto espectacular, una cuádruple granja de brujas capaz de generar 36k/h de redstone.",
     image: "/images/news/salem-project.png",
-    alt: "Proyecto Salem - Granja de brujas"
+    alt: "Proyecto Salem - Granja de brujas",
+    hrefButton: "https://www.youtube.com/playlist?list=PLTa5p56dEkq8roAe58mMPWC8nbD3ammd1"
   },
   {
     title: "ALMACEN PRINCIPAL",
     description: "Aurigas empezó a desarrollar su almacén principal en stream, la idea es que sea full codificado con la posibiliad de poner un monton de perfiericos!.",
     image: "/images/news/main-storage.png",
-    alt: "Almacén Principal"
+    alt: "Almacén Principal",
+    hrefButton: "https://www.youtube.com/playlist?list=PLTa5p56dEkq_-gSWH8Ir1UyPIp_w2inB6"
   },
   {
     title: "AGUA EN EL NETHER",
     description: "Aurigas a conseguido agua en el nether a través de mecánicas y bugs super interesantes, si quieres saber como lo hizo accede al stream destacado.",
     image: "/images/news/wather-nether.png",
-    alt: "Agua en el Nether"
+    alt: "Agua en el Nether",
+    hrefButton: "https://www.twitch.tv/videos/2331003160"
   },
   {
     title: "PROYECTO YUKON FINALIZADO",
     description: "Despues de muchos streams y meses de trabajo, Aurigas da por finalizada la granja de oro mas potente en SSP.",
     image: "/images/news/yukon-project.jpeg",
-    alt: "Proyecto - Yukon"
+    alt: "Proyecto - Yukon",
+    hrefButton: "/proyectos"
   }
 ];
