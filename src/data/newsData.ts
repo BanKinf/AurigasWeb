373f17657edfc1bd770b90b851a92236a6e197a3
_ignacio.dev_ <protec83007@gmail.com>
2025-03-10 12:39:14 -0300

    home page

---
+++ src/data/newsData.ts
@@ -0,0 +1,26 @@
+export const news = [
+    {
+    title: "PROYECTO SALEM",
+    description: "Aurigas a dado comienzo a un proyecto espectacular, una cuádruple granja de brujas capaz de generar 36k/h de redstone.",
+    image: "/images/news/salem-project.png",
+    alt: "Proyecto Salem - Granja de brujas"
+  },
+  {
+    title: "ALMACEN PRINCIPAL",
+    description: "Aurigas empezó a desarrollar su almacén principal en stream, la idea es que sea full codificado con la posibiliad de poner un monton de perfiericos!.",
+    image: "/images/news/main-storage.png",
+    alt: "Almacén Principal"
+  },
+  {
+    title: "AGUA EN EL NETHER",
+    description: "Aurigas a conseguido agua en el nether a través de mecánicas y bugs super interesantes, si quieres saber como lo hizo accede al stream destacado.",
+    image: "/images/news/wather-nether.png",
+    alt: "Agua en el Nether"
+  },
+  {
+    title: "PROYECTO YUKON FINALIZADO",
+    description: "Despues de muchos streams y meses de trabajo, Aurigas da por finalizada la granja de oro mas potente en SSP.",
+    image: "/images/news/yukon-project.jpeg",
+    alt: "Proyecto - Yukon"
+  }
+];
