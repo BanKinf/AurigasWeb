4dc7f459560281293ace5a77a0c41e00f8a5de52
_ignacio.dev_ <protec83007@gmail.com>
2025-03-11 18:55:13 -0300

    add new page

---
+++ src/data/faqsData.ts
@@ -0,0 +1,30 @@
+export const faqs = [
+    {
+        question: "¿Por qué no subes videos?",
+        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y el hecho de que siempre priorizo la calidad antes que la cantidad."
+    },
+    {
+        question: "¿Qué paso con LastHope?",
+        answer: "LastHope SMP cerró en mayo de 2020 y desde entonces no he vuelto a jugar en dicho servidor, lo más probable es que nunca vuelva a abrir sus puertas."
+    },
+    {
+        question: "¿Cuándo acabes el mundo lo dejarás para descargar?",
+        answer: "Para mi el concepto de acabar un mundo no tiene sentido en la modalidad de Minecraft que me gusta jugar, mi survival no tiene un final como tal, mientras siga disfrutando de este juego mi mapa seguirá en pie."
+    },
+    {
+        question: "¿Por qué no tienes netherite?",
+        answer: "No me gusta usar netherite porque lo considero un material demasiado caro de conseguir para la poca mejora que ofrece respecto al diamante, además es un material no renovable, y el diamante si lo es :)"
+    },
+    {
+        question: "¿Has abandonado YouTube?",
+        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y el hecho de que siempre priorizo la calidad antes que la cantidad."
+    },
+    {
+        question: "¿Por qué no usas litematica?",
+        answer: "Me gusta mantener el juego lo más puro posible y aunque no me cierro la puerta a usar mods, no me gusta abusar de ellos, por eso, pese a que no estoy contra de litematica, a mi personalmente no me gusta usarlo en mi mundo."
+    },
+    {
+        question: "¿Comó colocas bloques tan rapido?",
+        answer: 'Para poner bloques asi de rapido utilizo una tecla asignada al click derecho, es decir en el menu de configuracion en vez de tener el click derecho asignado para colocar bloques tengo la tecla R en su lugar y asi manteniendola puedo poner muchos bloques por segundo, si quieres saber mas mira este clip: <iframe src="https://clips.twitch.tv/embed?clip=FaithfulDarkBeefOneHand-wwfFRtsnAe5GGYso&parent=aurigas.es" frameborder="0" allowfullscreen="true" scrolling="no" height="250" width="460" class="mt-4"></iframe>'
+    }
+];
