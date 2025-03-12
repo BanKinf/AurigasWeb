4e70f720f2e1647028863bad2d706396aa11a521
_ignacio.dev_ <protec83007@gmail.com>
2025-03-12 13:11:14 -0300

    add new questions

--- src/data/faqsData.ts
+++ src/data/faqsData.ts
@@ -1,30 +1,42 @@
 export const faqs = [
     {
         question: "¿Por qué no subes videos?",
-        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y el hecho de que siempre priorizo la calidad antes que la cantidad."
+        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y al hecho de que siempre priorizo la calidad antes que la cantidad."
     },
     {
-        question: "¿Qué paso con LastHope?",
+        question: "¿Qué pasó con LastHope?",
         answer: "LastHope SMP cerró en mayo de 2020 y desde entonces no he vuelto a jugar en dicho servidor, lo más probable es que nunca vuelva a abrir sus puertas."
     },
     {
         question: "¿Cuándo acabes el mundo lo dejarás para descargar?",
-        answer: "Para mi el concepto de acabar un mundo no tiene sentido en la modalidad de Minecraft que me gusta jugar, mi survival no tiene un final como tal, mientras siga disfrutando de este juego mi mapa seguirá en pie."
+        answer: "Para mí, el concepto de acabar un mundo no tiene sentido en la modalidad de Minecraft que me gusta jugar. Mi survival no tiene un final como tal, mientras siga disfrutando de este juego mi mapa seguirá en pie."
     },
     {
         question: "¿Por qué no tienes netherite?",
-        answer: "No me gusta usar netherite porque lo considero un material demasiado caro de conseguir para la poca mejora que ofrece respecto al diamante, además es un material no renovable, y el diamante si lo es :)"
+        answer: "No me gusta usar netherite porque lo considero un material demasiado caro de conseguir para la poca mejora que ofrece respecto al diamante. Además, es un material no renovable, y el diamante sí lo es :)."
     },
     {
         question: "¿Has abandonado YouTube?",
-        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y el hecho de que siempre priorizo la calidad antes que la cantidad."
+        answer: "No, el canal de YouTube sigue en pie, pero la frecuencia de subida de videos es baja debido al trabajo que estos me llevan y al hecho de que siempre priorizo la calidad antes que la cantidad."
     },
     {
-        question: "¿Por qué no usas litematica?",
-        answer: "Me gusta mantener el juego lo más puro posible y aunque no me cierro la puerta a usar mods, no me gusta abusar de ellos, por eso, pese a que no estoy contra de litematica, a mi personalmente no me gusta usarlo en mi mundo."
+        question: "¿Por qué no usas Litematica?",
+        answer: "Me gusta mantener el juego lo más puro posible y, aunque no me cierro la puerta a usar mods, no me gusta abusar de ellos. Por eso, pese a que no estoy en contra de Litematica, a mí personalmente no me gusta usarlo en mi mundo."
     },
     {
-        question: "¿Comó colocas bloques tan rapido?",
-        answer: "Para poner bloques asi de rapido utilizo una tecla asignada al click derecho, es decir en el menu de configuracion en vez de tener el click derecho asignado para colocar bloques tengo la tecla R en su lugar y asi manteniendola puedo poner muchos bloques por segundo, si quieres saber mas entra a <a href='https://www.twitch.tv/aurigas/clip/FaithfulDarkBeefOneHand-wwfFRtsnAe5GGYso' target='_blank' rel='noopener noreferrer' class='text-blue-700 hover:border-b-1'>este clip</a>."
+        question: "¿Cómo colocas bloques tan rápido?",
+        answer: "Para poner bloques así de rápido utilizo una tecla asignada al clic derecho. Es decir, en el menú de configuración, en vez de tener el clic derecho asignado para colocar bloques, tengo la tecla R en su lugar, y así, manteniéndola, puedo poner muchos bloques por segundo. Si quieres saber más, entra a <a href='https://www.twitch.tv/aurigas/clip/FaithfulDarkBeefOneHand-wwfFRtsnAe5GGYso' target='_blank' rel='noopener noreferrer' class='text-blue-700 hover:border-b-1'>este clip</a>."
+    },
+    {
+        question: "¿Cómo picas tan rápido?",
+        answer: "Estoy utilizando un pico de diamante con eficiencia 5 y, además, tengo un beacon con prisa minera 2. Por esa razón, puedo instaminar bloques como la stone y la diorita."
+    },
+    {
+        question: "¿Qué música utilizas en tus directos?",
+        answer: "Yo en el fondo de mis directos tengo música lofi que se genera automáticamente con IA. Si quieres saber más, visita <a href='https://lofigenerator.com/' target='_blank' rel='noopener noreferrer' class='text-blue-700 hover:border-b-1'>esta web</a>."
+    },
+    {
+        question: "¿Cómo stackeas shulkers?",
+        answer: "Yo lo que utilizo para stackear shulker boxes es una rule del Carpet Mod que tienes disponible <a href='https://modrinth.com/mod/carpet' target='_blank' rel='noopener noreferrer' class='text-blue-700 hover:border-b-1'>aquí</a>. La rule se llama StackableShulkerBoxes y la tengo en la opción de stackeo a 64."
     }
 ];
