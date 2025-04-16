4dcb0962d6d8ebf8104d158e67046a1991a6aa4a
_ignacio.dev_ <protec83007@gmail.com>
2025-04-16 18:37:48 -0300

    add spookydex data and images

---
+++ src/data/SpookydexData.ts
@@ -0,0 +1,572 @@
+export interface SpookydexDataInterface {
+    name: string
+    id: string
+    image: string
+    probability: number
+    obtained: boolean
+    when?: number
+    usedFarm?: boolean
+    verion?: string
+    clip?: string
+};
+
+export const SpookydexDataArchive: SpookydexDataInterface[] = [
+    {
+        name: "Wither Skeleton Pumpkin",
+        id: "wither-skeleton-pumpkin",
+        image: "/images/spookydex/wither-skeleton-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/FS3zY9F6eDQ"
+    },
+    {
+        name: "Wither Skeleton Jack O'Lantern",
+        id: "wither-skeleton-jack-o-lantern",
+        image: "/images/spookydex/wither-skeleton-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/hMMgL57uOMo"
+    },
+    {
+        name: "Zombified Piglin Pumpkin",
+        id: "zombified-piglin-pumpkin",
+        image: "/images/spookydex/zombified-piglin-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/L_8qfeoDy-0"
+    },
+    {
+        name: "Zombified Piglin Jack O'Lantern",
+        id: "zombified-piglin-jack-o-lantern",
+        image: "/images/spookydex/zombified-piglin-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/QFkvKl9fOSg"
+    },
+    {
+        name: "Zombified Piglin Baby Pumpkin",
+        id: "zombified-piglin-baby-pumpkin",
+        image: "/images/spookydex/zombified-piglin-baby-pumpkin.webp",
+        probability: 1.125,
+        obtained: true,
+        when: 2020,
+        usedFarm: true,
+        verion: "1.16",
+        clip: "https://youtu.be/BxIxgT22xFs"
+    },
+    {
+        name: "Zombified Piglin Baby Jack O'Lantern",
+        id: "zombified-piglin-baby-jack-o-lantern",
+        image: "/images/spookydex/zombified-piglin-baby-jack-o-lantern.webp",
+        probability: 0.125,
+        obtained: true,
+        when: 2020,
+        usedFarm: true,
+        verion: "1.16",
+        clip: "https://youtu.be/c96DGa5XUZw"
+    },
+    {
+        name: "Zombified Piglin Chicken Jockey Pumpkin",
+        id: "zombified-piglin-chicken-jockey-pumpkin",
+        image: "/images/spookydex/zombified-piglin-chicken-jockey-pumpkin.webp",
+        probability: 0.05625,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: "https://youtu.be/xg9KhPeMAKQ"
+    },
+    {
+        name: "Zombified Piglin Chicken Jockey Jack O'Lantern",
+        id: "zombified-piglin-chicken-jockey-jack-o-lantern",
+        image: "/images/spookydex/zombified-piglin-chicken-jockey-jack-o-lantern.webp",
+        probability: 0.00625,
+        obtained: true,
+        when: 2023,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/IRdyMsaJHKI"
+    },
+    {
+        name: "Zombie Pumpkin",
+        id: "zombie-pumpkin",
+        image: "/images/spookydex/zombie-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/8aVDcI84B4o"
+    },
+    {
+        name: "Zombie Jack O'Lantern",
+        id: "zombie-jack-o-lantern",
+        image: "/images/spookydex/zombie-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/xL3dq2fe_5U"
+    },
+    {
+        name: "Zombie Baby Pumpkin",
+        id: "zombie-baby-pumpkin",
+        image: "/images/spookydex/zombie-baby-pumpkin.webp",
+        probability: 1.125,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/vG84kO6eYKs"
+    },
+    {
+        name: "Zombie Baby Jack O'Lantern",
+        id: "zombie-baby-jack-o-lantern",
+        image: "/images/spookydex/zombie-baby-jack-o-lantern.webp",
+        probability: 0.125,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/FndTRUnEDXY"
+    },
+    {
+        name: "Zombie Villager Pumpkin",
+        id: "zombie-villager-pumpkin",
+        image: "/images/spookydex/zombie-villager-pumpkin.webp",
+        probability: 1.125,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/vLkvsJKekTM"
+    },
+    {
+        name: "Zombie Villager Jack O'Lantern",
+        id: "zombie-villager-jack-o-lantern",
+        image: "/images/spookydex/zombie-villager-jack-o-lantern.webp",
+        probability: 0.125,
+        obtained: true,
+        when: 2021,
+        usedFarm: true,
+        verion: "1.17",
+        clip: "https://youtu.be/yDAfO5xbgEk"
+    },
+    //Falta el clip
+    {
+        name: "Baby Zombie Villager Pumpkin",
+        id: "baby-zombie-villager-pumpkin",
+        image: "/images/spookydex/baby-zombie-villager-pumpkin.webp",
+        probability: 0.05625,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: ""
+    },
+    //Falta el clip
+    {
+        name: "Baby Zombie Villager Jack O'Lantern",
+        id: "baby-zombie-villager-jack-o-lantern",
+        image: "/images/spookydex/baby-zombie-villager-jack-o-lantern.webp",
+        probability: 0.00625,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: ""
+    },
+    {
+        name: "Zombie Chicken Jockey Pumpkin",
+        id: "zombie-chicken-jockey-pumpkin",
+        image: "/images/spookydex/zombie-chicken-jockey-pumpkin.webp",
+        probability: 0.05625,
+        obtained: true,
+        when: 2021,
+        usedFarm: true,
+        verion: "1.17",
+        clip: "https://youtu.be/79KemiRsijw"
+    },
+    {
+        name: "Zombie Chicken Jockey Jack O'Lantern",
+        id: "zombie-chicken-jockey-jack-o-lantern",
+        image: "/images/spookydex/zombie-chicken-jockey-jack-o-lantern.webp",
+        probability: 0.00625,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: "https://youtu.be/QulW0I5UcEQ"
+    },
+    {
+        name: "Chicken Jockey Zombie Villager Pumpkin",
+        id: "chicken-jockey-zombie-villager-pumpkin",
+        image: "/images/spookydex/chicken-jockey-zombie-villager-pumpkin.webp",
+        probability: 0.0028125,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: "https://youtu.be/LAxEu4ZncLM"
+    },
+    {
+        name: "Chicken Jockey Zombie Villager Jack O'Lantern",
+        id: "chicken-jockey-zombie-villager-jack-o-lantern",
+        image: "/images/spookydex/chicken-jockey-zombie-villager-jack-o-lantern.webp",
+        probability: 0.0003125,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: "https://youtu.be/1G7LxDQmkcM"
+    },
+    {
+        name: "Skeleton Pumpkin",
+        id: "skeleton-pumpkin",
+        image: "/images/spookydex/skeleton-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/_IwbO78nmu4"
+    },
+    {
+        name: "Skeleton Jack O'Lantern",
+        id: "skeleton-jack-o-lantern",
+        image: "/images/spookydex/skeleton-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/8WhFrL5KkhY"
+    },
+    {
+        name: "Skeleton Spider Jockey Pumpkin",
+        id: "skeleton-spider-jockey-pumpkin",
+        image: "/images/spookydex/skeleton-spider-jockey-pumpkin.webp",
+        probability: 0.225,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/8ag7ezUtsHc"
+    },
+    {
+        name: "Skeleton Spider Jockey Jack O'Lantern",
+        id: "skeleton-spider-jockey-jack-o-lantern",
+        image: "/images/spookydex/skeleton-spider-jockey-jack-o-lantern.webp",
+        probability: 0.025,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/u2Gtn9-NdIo"
+    },
+    {
+        name: "Skeleton Horseman Pumpkin",
+        id: "skeleton-horseman-pumpkin",
+        image: "/images/spookydex/skeleton-horseman-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/7ch5FND4SMM"
+    },
+    {
+        name: "Skeleton Horseman Jack O'Lantern",
+        id: "skeleton-horseman-jack-o-lantern",
+        image: "/images/spookydex/skeleton-horseman-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2023,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/8_HrsoYDMB8"
+    },
+    {
+        name: "Husk Pumpkin",
+        id: "husk-pumpkin",
+        image: "/images/spookydex/husk-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/0KjWC3i0toE"
+    },
+    {
+        name: "Husk Jack O'Lantern",
+        id: "husk-jack-o-lantern",
+        image: "/images/spookydex/husk-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/-6v83S-yjLA"
+    },
+    {
+        name: "Husk Baby Pumpkin",
+        id: "husk-baby-pumpkin",
+        image: "/images/spookydex/husk-baby-pumpkin.webp",
+        probability: 1.125,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/vZSAEMmrvWA"
+    },
+    {
+        name: "Husk Baby Jack O'Lantern",
+        id: "husk-baby-jack-o-lantern",
+        image: "/images/spookydex/husk-baby-jack-o-lantern.webp",
+        probability: 0.125,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/zKaKoUhNjo0"
+    },
+    {
+        name: "Husk Chicken Jockey Pumpkin",
+        id: "husk-chicken-jockey-pumpkin",
+        image: "/images/spookydex/husk-chicken-jockey-pumpkin.webp",
+        probability: 0.05625,
+        obtained: true,
+        when: 2021,
+        usedFarm: true,
+        verion: "1.17",
+        clip: "https://youtu.be/L33zQEPFZkc"
+    },
+    {
+        name: "Husk Chicken Jockey Jack O'Lantern",
+        id: "husk-chicken-jockey-jack-o-lantern",
+        image: "/images/spookydex/husk-chicken-jockey-jack-o-lantern.webp",
+        probability: 0.00625,
+        obtained: true,
+        when: 2021,
+        usedFarm: true,
+        verion: "1.17",
+        clip: "https://youtu.be/0kBVovLotrg"
+    },
+    {
+        name: "Stray Pumpkin",
+        id: "stray-pumpkin",
+        image: "/images/spookydex/stray-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/10X3rCjL_WY"
+    },
+    {
+        name: "Stray Jack O'Lantern",
+        id: "stray-jack-o-lantern",
+        image: "/images/spookydex/stray-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/joYe4bH6-oA"
+    },
+    {
+        name: "Stray Spider Jockey Pumpkin",
+        id: "stray-spider-jockey-pumpkin",
+        image: "/images/spookydex/stray-spider-jockey-pumpkin.webp",
+        probability: 0.225,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/ZrNwnrIgClo"
+    },
+    {
+        name: "Stray Spider Jockey Jack O'Lantern",
+        id: "stray-spider-jockey-jack-o-lantern",
+        image: "/images/spookydex/stray-spider-jockey-jack-o-lantern.webp",
+        probability: 0.025,
+        obtained: true,
+        when: 2022,
+        usedFarm: false,
+        verion: "1.18",
+        clip: "https://youtu.be/FVMLCxIM1uw"
+    },
+    {
+        name: "Stray Skeleton Horseman Pumpkin",
+        id: "stray-skeleton-horseman-pumpkin",
+        image: "/images/spookydex/stray-skeleton-horseman-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2023,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/0JNnsefQHjc"
+    },
+    {
+        name: "Stray Skeleton Horseman Jack O'Lantern",
+        id: "stray-skeleton-horseman-jack-o-lantern",
+        image: "/images/spookydex/stray-skeleton-horseman-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2023,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/ytGNpjeDT4I"
+    },
+    {
+        name: "Drowned Pumpkin",
+        id: "drowned-pumpkin",
+        image: "/images/spookydex/drowned-pumpkin.webp",
+        probability: 22.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/t-NPGFSnJBk"
+    },
+    {
+        name: "Drowned Jack O'Lantern",
+        id: "drowned-jack-o-lantern",
+        image: "/images/spookydex/drowned-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/0FP0h9uBrYU"
+    },
+    {
+        name: "Drowned Baby Pumpkin",
+        id: "drowned-baby-pumpkin",
+        image: "/images/spookydex/drowned-baby-pumpkin.webp",
+        probability: 1.125,
+        obtained: true,
+        when: 2020,
+        usedFarm: false,
+        verion: "1.16",
+        clip: "https://youtu.be/aG8Zhnr6ZZI"
+    },
+    {
+        name: "Drowned Baby Jack O'Lantern",
+        id: "drowned-baby-jack-o-lantern",
+        image: "/images/spookydex/drowned-baby-jack-o-lantern.webp",
+        probability: 0.125,
+        obtained: true,
+        when: 2021,
+        usedFarm: false,
+        verion: "1.17",
+        clip: "https://youtu.be/26HkdinBrCs"
+    },
+    {
+        name: "Drowned Chicken Jockey Pumpkin",
+        id: "drowned-chicken-jockey-pumpkin",
+        image: "/images/spookydex/drowned-chicken-jockey-pumpkin.webp",
+        probability: 0.05625,
+        obtained: true,
+        when: 2021,
+        usedFarm: true,
+        verion: "1.17",
+        clip: "https://youtu.be/Z5oyDkzQhIs"
+    },
+    {
+        name: "Drowned Chicken Jockey Jack O'Lantern",
+        id: "drowned-chicken-jockey-jack-o-lantern",
+        image: "/images/spookydex/drowned-chicken-jockey-jack-o-lantern.webp",
+        probability: 0.00625,
+        obtained: true,
+        when: 2022,
+        usedFarm: true,
+        verion: "1.18",
+        clip: "https://youtu.be/uoZ2a7jMfek"
+    },
+    {
+        name: "Strider Jockey Zombified Piglin Pumpkin",
+        id: "strider-jockey-zombified-piglin-pumpkin",
+        image: "/images/spookydex/strider-jockey-zombified-piglin-pumpkin.webp",
+        probability: 0.8415,
+        obtained: true,
+        when: 2024,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/1zSSp0lA9kE"
+    },
+    {
+        name: "Strider Jockey Zombified Piglin Jack O'Lantern",
+        id: "strider-jockey-zombified-piglin-jack-o-lantern",
+        image: "/images/spookydex/strider-jockey-zombified-piglin-jack-o-lantern.webp",
+        probability: 0.0825,
+        obtained: true,
+        when: 2024,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/Z31smic3vXU"
+    },
+    {
+        name: "Strider Jockey Baby Zombified Piglin Pumpkin",
+        id: "strider-jockey-baby-zombified-piglin-pumpkin",
+        image: "/images/spookydex/strider-jockey-baby-zombified-piglin-pumpkin.webp",
+        probability: 0.0421,
+        obtained: true,
+        when: 2024,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/Z-IV1XLebjs"
+    },
+    {
+        name: "Strider Jockey Baby Zombified Piglin Jack O'Lantern",
+        id: "strider-jockey-baby-zombified-piglin-jack-o-lantern",
+        image: "/images/spookydex/strider-jockey-baby-zombified-piglin-jack-o-lantern.webp",
+        probability: 0.0041,
+        obtained: true,
+        when: 2024,
+        usedFarm: true,
+        verion: "1.19",
+        clip: "https://youtu.be/O1edwnbvfBw"
+    },
+    {
+        name: "Bogged Pumpkin",
+        id: "bogged-pumpkin",
+        image: "/images/spookydex/bogged-pumpkin.webp",
+        probability: 22.5,
+        obtained: false,
+    },
+    {
+        name: "Bogged Jack O'Lantern",
+        id: "bogged-jack-o-lantern",
+        image: "/images/spookydex/bogged-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: false,
+    },
+    {
+        name: "Wither Skeleton Horseman Pumpkin",
+        id: "wither-skeleton-horseman-pumpkin",
+        image: "/images/spookydex/wither-skeleton-horseman-pumpkin.webp",
+        probability: 22.5,
+        obtained: false,
+    },
+    {
+        name: "Wither Skeleton Horseman Jack O'Lantern",
+        id: "wither-skeleton-horseman-jack-o-lantern",
+        image: "/images/spookydex/wither-skeleton-horseman-jack-o-lantern.webp",
+        probability: 2.5,
+        obtained: false,
+    }
+];
